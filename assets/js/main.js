c421e5abe7d2787853eb0eac393673bf09140ff9
Your Name <you@example.com>
2023-11-09 13:18:51 +0300

    alisher

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,54 @@
  
-const bars = document.querySelector(".fa-bars"),
-      navbarNav = document.querySelector(".navbar_nav"),
-      nav = navbarNav.querySelectorAll(".navbar_nav a");
+// const bars = document.querySelector(".fa-bars"),
+//       navbarNav = document.querySelector(".navbar_nav"),
+//       nav = navbarNav.querySelectorAll(".navbar_nav a");
 
-      bars.addEventListener("click", () =>{
-        navbarNav.classList.toggle("show");
-        navbarNav.classList.remove("hide");
+//       bars.addEventListener("click", () =>{
+//         navbarNav.classList.toggle("show");
+//         navbarNav.classList.remove("hide");
       
-      } );
-      navbarNav.addEventListener("click", (e) =>{
-        e.target
-        nav.forEach((item) =>{
-          if(e.target == item   ){
-            setTimeout(() =>{
-               navbarNav.classList.add("hide");
-            }, 500)
-          }
-        })
-      });
+//       } );
+//       navbarNav.addEventListener("click", (e) =>{
+//         e.target
+//         nav.forEach((item) =>{
+//           if(e.target == item   ){
+//             setTimeout(() =>{
+//                navbarNav.classList.add("hide");
+//             }, 500)
+//           }
+//         })
+//       });
 
       // setTimeout(() => {
       //   navbarNav.classList.add("hide")
       // }, 3500);
+
+      const addNavigation = document.querySelector('.add__navigation');
+const bars = document.querySelector('.fa-bars'),
+    closeBars = document.querySelector('.close__bars');
+
+bars.addEventListener('click', ()=>{
+    addNavigation.classList.remove('hide');
+    addNavigation.classList.add('roll-in-right', 'show');
+    
+});
+closeBars.addEventListener('click', ()=>{
+    addNavigation.classList.remove('roll-in-right', 'show');
+    addNavigation.classList.add('hide');
+});
+addNavigation.addEventListener('click', (Event)=>{
+    
+    if(Event.target){
+        function barsShow(){
+            setTimeout(() => {
+                addNavigation.classList.add('hide');
+                addNavigation.classList.remove('show','fade'); 
+                clearTimeout(timirBars);
+            }, 1000);     
+
+        }
+ 
+        barsShow();
+ 
+    }
+});
